226421875a566b59634b7f147d2369dfe10cc6ab
chaasjes <chaasjes@lumc.nl>
2024-07-11 10:15:43 +0200

    Make zernike standard coefficients analysis globally accessible

--- visisipy/analysis/__init__.py
+++ visisipy/analysis/__init__.py
@@ -1,4 +1,7 @@
 from visisipy.analysis.raytracing import raytrace
 from visisipy.analysis.refraction import refraction
+from visisipy.analysis.zernike_standard_coefficients import (
+    zernike_standard_coefficients,
+)
 
-__all__ = ("raytrace", "refraction")
+__all__ = ("raytrace", "refraction", "zernike_standard_coefficients")
