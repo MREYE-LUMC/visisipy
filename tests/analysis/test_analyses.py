75b0b68d60d424ece07d758145cc7a6ee00e4023
chaasjes <chaasjes@lumc.nl>
2024-07-11 10:19:41 +0200

    Add tests for global analysis module
    Tests do not test the backend, only the resolution of the backend analysis implementation.

---
+++ tests/analysis/test_analyses.py
@@ -0,0 +1,59 @@
+import visisipy
+import visisipy._backend
+
+
+class MockAnalysis:
+    def raytrace(
+        self,
+        coordinates,
+        wavelengths,
+        field_type,
+        pupil,
+    ):
+        return None, None
+
+    def zernike_standard_coefficients(
+        self, field_coordinate, wavelength, field_type, sampling, maximum_term
+    ):
+        return None, None
+
+    def refraction(
+        self,
+        use_higher_order_aberrations,
+        field_coordinate,
+        wavelength,
+        pupil_diameter,
+        field_type,
+    ):
+        return None, None
+
+
+class MockBackend:
+    analysis = MockAnalysis()
+
+
+def test_raytracing_analysis(monkeypatch):
+    monkeypatch.setattr(visisipy._backend, "_BACKEND", MockBackend)
+
+    assert visisipy.analysis.raytrace(coordinates=[(0, 0)]) is None
+    assert visisipy.analysis.raytrace(coordinates=[(0, 0)], return_raw_result=True) == (
+        None,
+        None,
+    )
+
+
+def test_zernike_standard_coefficients_analysis(monkeypatch):
+    monkeypatch.setattr(visisipy._backend, "_BACKEND", MockBackend)
+
+    assert visisipy.analysis.zernike_standard_coefficients() is None
+    assert visisipy.analysis.zernike_standard_coefficients(return_raw_result=True) == (
+        None,
+        None,
+    )
+
+
+def test_refraction_analysis(monkeypatch):
+    monkeypatch.setattr(visisipy._backend, "_BACKEND", MockBackend)
+
+    assert visisipy.analysis.refraction() is None
+    assert visisipy.analysis.refraction(return_raw_result=True) == (None, None)
