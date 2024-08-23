740340553c0fbee9b10bbb42a4c4cde1c3fe732f
crnh <30109443+crnh@users.noreply.github.com>
2024-08-23 12:00:15 +0200

    Add get_oss function to get OpticStudioSystem from the backend

--- visisipy/backend.py
+++ visisipy/backend.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import importlib
 from abc import ABC, abstractmethod
+from enum import Enum
 from typing import TYPE_CHECKING, Literal
 from warnings import warn
 
@@ -10,12 +11,13 @@ if TYPE_CHECKING:
     from os import PathLike
 
     from pandas import DataFrame
+    from zospy.zpcore import OpticStudioSystem
 
-    from visisipy.analysis import FourierPowerVectorRefraction
     from visisipy.models import BaseEye, EyeModel
+    from visisipy.refraction import FourierPowerVectorRefraction
 
 _BACKEND: BaseBackend | None = None
-_DEFAULT_BACKEND: Literal["opticstudio"] = "opticstudio"
+_DEFAULT_BACKEND: Backend | str = "opticstudio"
 
 
 class _classproperty(property):  # noqa: N801
@@ -61,7 +63,25 @@ class BaseBackend(ABC):
     def save_model(cls, filename: str | PathLike | None = None) -> None: ...
 
 
-def set_backend(backend: Literal["opticstudio"] = "opticstudio", **kwargs) -> None:
+class Backend(str, Enum):
+    OPTICSTUDIO = "opticstudio"
+
+
+def set_backend(backend: Backend | str = Backend.OPTICSTUDIO, **kwargs) -> None:
+    """Set the backend to use for optical simulations.
+
+    Parameters
+    ----------
+    backend : Backend | str
+        The backend to use. Must be one of the values in the `Backend` enum. Defaults to `Backend.OPTICSTUDIO`.
+    kwargs
+        Additional keyword arguments to pass to the backend's `initialize` method.
+
+    Raises
+    ------
+    ValueError
+        If an invalid backend is specified.
+    """
     global _BACKEND  # noqa: PLW0603
 
     if _BACKEND is not None:
@@ -70,7 +90,7 @@ def set_backend(backend: Literal["opticstudio"] = "opticstudio", **kwargs) -> No
             f"Reconfiguring the backend is not recommended and may cause issues."
         )
 
-    if backend == "opticstudio":
+    if backend == Backend.OPTICSTUDIO:
         os_backend = importlib.import_module("visisipy.opticstudio.backend")
         _BACKEND = os_backend.OpticStudioBackend
         _BACKEND.initialize(**kwargs)
@@ -79,7 +99,31 @@ def set_backend(backend: Literal["opticstudio"] = "opticstudio", **kwargs) -> No
 
 
 def get_backend() -> BaseBackend:
+    """Get the current backend.
+
+    Returns
+    -------
+    BaseBackend
+        The current backend.
+    """
     if _BACKEND is None:
         set_backend(_DEFAULT_BACKEND)
 
     return _BACKEND
+
+
+def get_oss() -> OpticStudioSystem | None:
+    """
+    Get the OpticStudioSystem instance from the current backend.
+
+    Returns
+    -------
+    OpticStudioSystem | None
+        The OpticStudioSystem instance if the current backend is the OpticStudio backend, otherwise `None`.
+    """
+    os_backend = importlib.import_module("visisipy.opticstudio.backend")
+
+    if _BACKEND is os_backend.OpicStudioBackend:
+        return os_backend._oss
+
+    return None
