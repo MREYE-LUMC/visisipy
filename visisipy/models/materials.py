47dc0bd5ea45ddecbaa34cc7175bd31ca4d2b111
crnh <30109443+crnh@users.noreply.github.com>
2025-04-11 14:58:31 +0200

    Add material classes for all separate wavelengths specified by Navarro et al.

--- visisipy/models/materials.py
+++ visisipy/models/materials.py
@@ -1,8 +1,7 @@
 from dataclasses import dataclass, field
+from typing import Callable
 
 __all__ = ("EyeMaterials", "MaterialModel", "NavarroMaterials")
-
-from typing import Callable
 
 
 @dataclass
@@ -57,7 +56,7 @@ def _material_model_factory(refractive_index, abbe_number, partial_dispersion) -
 
 @dataclass
 class NavarroMaterials(EyeMaterials):
-    """Material parameters of an eye, according to the Navarro model.
+    """Material parameters of an eye, according to the Navarro model [1]_.
 
     The Navarro model defines refractive indices at various wavelengths.
     These data were used to fit the material models in OpticStudio.
@@ -72,6 +71,12 @@ class NavarroMaterials(EyeMaterials):
         Refractive model of the crystalline lens.
     vitreous : MaterialModel
         Refractive model of the vitreous humour.
+
+    References
+    ----------
+    .. [1] Escudero-Sanz, I., & Navarro, R. (1999).
+       Off-axis aberrations of a wide-angle schematic eye model.
+       JOSA A, 16(8), 1881-1891. https://doi.org/10.1364/JOSAA.16.001881
     """
 
     cornea: MaterialModel = field(
@@ -100,5 +105,213 @@ class NavarroMaterials(EyeMaterials):
             refractive_index=1.33602751687,
             abbe_number=50.8796247462,
             partial_dispersion=0.0531865765832,
+        )
+    )
+
+
+@dataclass
+class NavarroMaterials458(EyeMaterials):
+    """Material parameters of an eye for a wavelength of 458 nm, according to the Navarro model [1]_.
+
+    Attributes
+    ----------
+    cornea : MaterialModel
+        Refractive model of the cornea.
+    aqueous : MaterialModel
+        Refractive model of the aqueous humour.
+    lens : MaterialModel
+        Refractive model of the crystalline lens.
+    vitreous : MaterialModel
+        Refractive model of the vitreous humour.
+
+    References
+    ----------
+    .. [1] Escudero-Sanz, I., & Navarro, R. (1999).
+       Off-axis aberrations of a wide-angle schematic eye model.
+       JOSA A, 16(8), 1881-1891. https://doi.org/10.1364/JOSAA.16.001881
+    """
+
+    cornea: MaterialModel = field(
+        default_factory=_material_model_factory(
+            refractive_index=1.3828,
+            abbe_number=0,
+            partial_dispersion=0,
+        )
+    )
+    aqueous: MaterialModel = field(
+        default_factory=_material_model_factory(
+            refractive_index=1.3445,
+            abbe_number=0,
+            partial_dispersion=0,
+        )
+    )
+    lens: MaterialModel = field(
+        default_factory=_material_model_factory(
+            refractive_index=1.4292,
+            abbe_number=0,
+            partial_dispersion=0,
+        )
+    )
+    vitreous: MaterialModel = field(
+        default_factory=_material_model_factory(
+            refractive_index=1.3428,
+            abbe_number=0,
+            partial_dispersion=0,
+        )
+    )
+
+
+@dataclass
+class NavarroMaterials543(EyeMaterials):
+    """Material parameters of an eye for a wavelength of 543 nm, according to the Navarro model [1]_.
+
+    Attributes
+    ----------
+    cornea : MaterialModel
+        Refractive model of the cornea.
+    aqueous : MaterialModel
+        Refractive model of the aqueous humour.
+    lens : MaterialModel
+        Refractive model of the crystalline lens.
+    vitreous : MaterialModel
+        Refractive model of the vitreous humour.
+
+    References
+    ----------
+    .. [1] Escudero-Sanz, I., & Navarro, R. (1999).
+       Off-axis aberrations of a wide-angle schematic eye model.
+       JOSA A, 16(8), 1881-1891. https://doi.org/10.1364/JOSAA.16.001881
+    """
+
+    cornea: MaterialModel = field(
+        default_factory=_material_model_factory(
+            refractive_index=1.3777,
+            abbe_number=0,
+            partial_dispersion=0,
+        )
+    )
+    aqueous: MaterialModel = field(
+        default_factory=_material_model_factory(
+            refractive_index=1.3391,
+            abbe_number=0,
+            partial_dispersion=0,
+        )
+    )
+    lens: MaterialModel = field(
+        default_factory=_material_model_factory(
+            refractive_index=1.4222,
+            abbe_number=0,
+            partial_dispersion=0,
+        )
+    )
+    vitreous: MaterialModel = field(
+        default_factory=_material_model_factory(
+            refractive_index=1.3377,
+            abbe_number=0,
+            partial_dispersion=0,
+        )
+    )
+
+
+@dataclass
+class NavarroMaterials589(EyeMaterials):
+    """Material parameters of an eye for a wavelength of 589.3 nm, according to the Navarro model [1]_.
+
+    Attributes
+    ----------
+    cornea : MaterialModel
+        Refractive model of the cornea.
+    aqueous : MaterialModel
+        Refractive model of the aqueous humour.
+    lens : MaterialModel
+        Refractive model of the crystalline lens.
+    vitreous : MaterialModel
+        Refractive model of the vitreous humour.
+
+    References
+    ----------
+    .. [1] Escudero-Sanz, I., & Navarro, R. (1999).
+       Off-axis aberrations of a wide-angle schematic eye model.
+       JOSA A, 16(8), 1881-1891. https://doi.org/10.1364/JOSAA.16.001881
+    """
+
+    cornea: MaterialModel = field(
+        default_factory=_material_model_factory(
+            refractive_index=1.376,
+            abbe_number=0,
+            partial_dispersion=0,
+        )
+    )
+    aqueous: MaterialModel = field(
+        default_factory=_material_model_factory(
+            refractive_index=1.3374,
+            abbe_number=0,
+            partial_dispersion=0,
+        )
+    )
+    lens: MaterialModel = field(
+        default_factory=_material_model_factory(
+            refractive_index=1.42,
+            abbe_number=0,
+            partial_dispersion=0,
+        )
+    )
+    vitreous: MaterialModel = field(
+        default_factory=_material_model_factory(
+            refractive_index=1.336,
+            abbe_number=0,
+            partial_dispersion=0,
+        )
+    )
+
+
+@dataclass
+class NavarroMaterials633(EyeMaterials):
+    """Material parameters of an eye for a wavelength of 632.8 nm, according to the Navarro model [1]_.
+
+    Attributes
+    ----------
+    cornea : MaterialModel
+        Refractive model of the cornea.
+    aqueous : MaterialModel
+        Refractive model of the aqueous humour.
+    lens : MaterialModel
+        Refractive model of the crystalline lens.
+    vitreous : MaterialModel
+        Refractive model of the vitreous humour.
+
+    References
+    ----------
+    .. [1] Escudero-Sanz, I., & Navarro, R. (1999).
+       Off-axis aberrations of a wide-angle schematic eye model.
+       JOSA A, 16(8), 1881-1891. https://doi.org/10.1364/JOSAA.16.001881
+    """
+
+    cornea: MaterialModel = field(
+        default_factory=_material_model_factory(
+            refractive_index=1.3747,
+            abbe_number=0,
+            partial_dispersion=0,
+        )
+    )
+    aqueous: MaterialModel = field(
+        default_factory=_material_model_factory(
+            refractive_index=1.336,
+            abbe_number=0,
+            partial_dispersion=0,
+        )
+    )
+    lens: MaterialModel = field(
+        default_factory=_material_model_factory(
+            refractive_index=1.4183,
+            abbe_number=0,
+            partial_dispersion=0,
+        )
+    )
+    vitreous: MaterialModel = field(
+        default_factory=_material_model_factory(
+            refractive_index=1.3347,
+            abbe_number=0,
+            partial_dispersion=0,
         )
     )
