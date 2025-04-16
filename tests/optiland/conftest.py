7474c5c3ef6e9cf183ce390178a3c0f588a388be
crnh <30109443+crnh@users.noreply.github.com>
2025-04-16 13:14:35 +0200

    Fit material models with refractive index and Abbe number only (#12)
    * Add script to fit material models
    
    * Update NavarroMaterials with fits in Optiland
    
    * Update tests
    
    * Add Optiland tests for NavarroMaterials

--- tests/optiland/conftest.py
+++ tests/optiland/conftest.py
@@ -32,6 +32,7 @@ def optic() -> Optic:
     """Fixture to create an Optic instance for testing."""
     system = Optic()
 
+    system.set_field_type("angle")
     system.add_field(0, 0)
     system.add_wavelength(0.543)
     system.set_aperture("EPD", 1.0)
