e4f3d2a4558ac5a989fc258e30b8bfb50f99419c
crnh <30109443+crnh@users.noreply.github.com>
2025-05-06 14:22:26 +0200

    Add support for non-Windows systems (#26)
    * Load visisipy.opticstudio only when on Windows
    
    * Add a custom loader to prevent importing ZOSPy and raise an exception explaining why ZOSPy can't be used
    
    * Add tests for ZOSPy import on Windows and non-Windows systems
    
    * Skip windows-only tests when running on Linux
    
    * Install ZOSPy only on Windows
    
    * Ensure ZOSPyFinder is only installed once
    
    * Prevent false matches for ZOSPyFinder
    
    Other packages with names starting with zospy are now not matched

--- visisipy/__init__.py
+++ visisipy/__init__.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+import platform
 from importlib.metadata import version
 
-from visisipy import analysis, models, opticstudio, optiland, plots, refraction, wavefront
+from visisipy import analysis, models, optiland, plots, refraction, wavefront
+from visisipy._zospy_loader import install_zospy_loader
 from visisipy.backend import get_backend, set_backend, update_settings
 from visisipy.models import (
     EyeGeometry,
@@ -12,7 +14,7 @@ from visisipy.models import (
     NavarroMaterials,
 )
 
-__all__ = (
+__all__ = [
     "EyeGeometry",
     "EyeMaterials",
     "EyeModel",
@@ -21,13 +23,20 @@ __all__ = (
     "analysis",
     "get_backend",
     "models",
-    "opticstudio",
     "optiland",
     "plots",
     "refraction",
     "set_backend",
     "update_settings",
     "wavefront",
-)
+]
+
+# The OpticStudio backend is only available on Windows
+if platform.system() == "Windows":
+    from visisipy import opticstudio
+
+    __all__ += ["opticstudio"]
 
 __version__ = version("visisipy")
+
+install_zospy_loader()
