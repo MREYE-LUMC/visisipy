10c224c6150c29153e87620b559c14b0deecd15a
crnh <30109443+crnh@users.noreply.github.com>
2025-05-20 14:49:51 +0200

    Document RMS HOA analysis

--- visisipy/analysis/zernike_standard_coefficients.py
+++ visisipy/analysis/zernike_standard_coefficients.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Literal, overload
+from typing import Any, Literal, TYPE_CHECKING, overload
 
 import numpy as np
 
@@ -149,10 +149,52 @@ def rms_hoa(
 ) -> tuple[float, Any]:
     """Calculates the root-mean-square (RMS) of higher-order aberrations (HOA) in the eye model.
 
-    By default,
+    By default, the Zernike orders from 3 to 8 are used. The RMS is calculated following the definition in the ANSI
+    Z80.28-2010 standard [1]_:
+
+    .. math:: RMS_{WFE} = \\sqrt{ \\sum_{n>1, all\\ m} (C_{n}^{m})^2 }
+
+    .. [1]: VC ANSI Z80.28-2010—Ophthalmics—Methods of Reporting Optical Aberrations of Eyes. (n.d.).
+            https://webstore.ansi.org/standards/ansi/vcansiz80282010
 
     Parameters
     ----------
+    model : EyeModel | None
+        The eye model to be used in the ray trace. If `None`, the current eye model will be used.
+    min_order : int
+        The minimum Zernike polynomial order to be included in the calculation. Defaults to 3.
+    max_order : int
+        The maximum Zernike polynomial order to be included in the calculation. Defaults to 8.
+    field_coordinate : tuple[float, float] | None, optional
+        The field coordinate for the Zernike calculation. When `None`, the first field in the backend is used.
+        Defaults to `None`.
+    wavelength : float | None, optional
+        The wavelength for the Zernike calculation. When `None`, the first wavelength in the backend is used.
+        Defaults to `None`.
+    field_type : Literal["angle", "object_height"], optional
+        The type of field to be used when setting the field coordinate. This parameter is only used when
+        `field_coordinate` is specified. Defaults to "angle".
+    sampling : SampleSize | str | int, optional
+        The sampling for the Zernike calculation. Defaults to 64.
+    maximum_term : int | None, optional
+        The maximum term for the Zernike calculation. If `None`, the maximum term is set to the largest term of
+        `max_order`. Defaults to `None`.
+    return_raw_result : bool, optional
+        Return the raw analysis result from the backend. Defaults to `False`.
+    backend : type[BaseBackend]
+        The backend to be used for the analysis. If not provided, the current backend is used.
+
+    Returns
+    -------
+    float
+        The root-mean-square (RMS) of higher-order aberrations (HOA) in the eye model.
+
+    Raises
+    ------
+    ValueError
+        If `min_order` or `max_order` is less than 0.
+        If `max_order` is less than or equal to `min_order`.
+        If `maximum_term` is less than the largest term of `max_order`.
     """
     if min_order < 0 or max_order < 0:
         raise ValueError("min_order and max_order must be greater than or equal to 0")
@@ -163,7 +205,7 @@ def rms_hoa(
     min_index, max_index = min_max_noll_index(min_order, max_order)
 
     if maximum_term is None:
-        maximum_term = max_order
+        maximum_term = max_index
     elif maximum_term < max_index:
         raise ValueError(f"maximum_term must be greater than or equal to the largest term of max_order: {max_index}")
 
