5b4a52415b06ed538fe9a548c1687193077794da
crnh <30109443+crnh@users.noreply.github.com>
2026-02-11 15:47:56 +0100

    Generate test data for analysis result tests

---
+++ scripts/test_data/analysis_tests.py
@@ -0,0 +1,179 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+import pandas as pd
+
+import visisipy
+
+if TYPE_CHECKING:
+    from visisipy.backend import BaseBackend
+    from visisipy.models.base import EyeModel
+
+
+class BaseAnalysisTest(ABC):
+    @abstractmethod
+    def run(self, model: EyeModel, backend: type[BaseBackend]) -> pd.DataFrame: ...
+
+    """Run the analysis to generate test data.
+
+    This method must return a dataframe with the results of the analysis.
+    Series are intentionally not supported, to make loading the results easier and more consistent across tests.
+    """
+
+
+class FFTPSFTest(BaseAnalysisTest):
+    def __init__(
+        self,
+        coordinate: tuple[float, float],
+        sampling: int,
+        wavelength: float,
+    ) -> None:
+        self.coordinate = coordinate
+        self.sampling = sampling
+        self.wavelength = wavelength
+
+    def run(self, model: EyeModel, backend: type[BaseBackend]) -> pd.DataFrame:
+        return visisipy.analysis.fft_psf(
+            model=model,
+            field_coordinate=self.coordinate,
+            field_type="angle",
+            sampling=self.sampling,
+            wavelength=self.wavelength,
+            backend=backend,
+        )
+
+
+class HuygensPSFTest(BaseAnalysisTest):
+    def __init__(
+        self,
+        coordinate: tuple[float, float],
+        pupil_sampling: int,
+        image_sampling: int,
+        wavelength: float,
+    ) -> None:
+        self.coordinate = coordinate
+        self.pupil_sampling = pupil_sampling
+        self.image_sampling = image_sampling
+        self.wavelength = wavelength
+
+    def run(self, model: EyeModel, backend: type[BaseBackend]) -> pd.DataFrame:
+        return visisipy.analysis.huygens_psf(
+            model=model,
+            field_coordinate=self.coordinate,
+            field_type="angle",
+            pupil_sampling=self.pupil_sampling,
+            image_sampling=self.image_sampling,
+            wavelength=self.wavelength,
+            backend=backend,
+        )
+
+
+class RayTraceTest(BaseAnalysisTest):
+    def __init__(
+        self,
+        coordinates: list[tuple[float, float]],
+        pupil: tuple[float, float],
+        sampling: int,
+        wavelength: float,
+    ) -> None:
+        self.coordinates = coordinates
+        self.pupil = pupil
+        self.sampling = sampling
+        self.wavelength = wavelength
+
+    def run(self, model: EyeModel, backend: type[BaseBackend]) -> pd.DataFrame:
+        return visisipy.analysis.raytrace(
+            model=model,
+            coordinates=self.coordinates,
+            field_type="angle",
+            wavelengths=[self.wavelength],
+            pupil=self.pupil,
+            backend=backend,
+        )
+
+
+class RefractionTest(BaseAnalysisTest):
+    def __init__(
+        self,
+        *,
+        coordinates: list[tuple[float, float]],
+        wavelength: float,
+        sampling: int,
+    ) -> None:
+        self.coordinates = coordinates
+        self.sampling = sampling
+        self.wavelength = wavelength
+
+    def run(self, model: EyeModel, backend: type[BaseBackend]) -> pd.DataFrame:
+        result: list[tuple[float, float, float, float, float]] = []
+
+        for coord in self.coordinates:
+            refraction = visisipy.analysis.refraction(
+                model=model,
+                field_coordinate=coord,
+                sampling=self.sampling,
+                wavelength=self.wavelength,
+                backend=backend,
+            )
+
+            result.append((coord[0], coord[1], refraction.M, refraction.J0, refraction.J45))
+
+        return pd.DataFrame(result, columns=["field_x", "field_y", "M", "J0", "J45"])
+
+
+class OPDMapTest(BaseAnalysisTest):
+    def __init__(
+        self,
+        *,
+        coordinate: tuple[float, float],
+        sampling: int,
+        wavelength: float,
+        remove_tilt: bool,
+    ) -> None:
+        self.coordinate = coordinate
+        self.sampling = sampling
+        self.wavelength = wavelength
+        self.remove_tilt = remove_tilt
+
+    def run(self, model: EyeModel, backend: type[BaseBackend]) -> pd.DataFrame:
+        return visisipy.analysis.opd_map(
+            model=model,
+            field_coordinate=self.coordinate,
+            wavelength=self.wavelength,
+            field_type="angle",
+            sampling=self.sampling,
+            remove_tilt=self.remove_tilt,
+            use_exit_pupil_shape=False,
+            backend=backend,
+        )
+
+
+class ZernikeStandardCoefficientsTest(BaseAnalysisTest):
+    def __init__(
+        self,
+        *,
+        coordinate: tuple[float, float],
+        sampling: int,
+        wavelength: float,
+        maximum_term: int,
+    ) -> None:
+        self.coordinate = coordinate
+        self.sampling = sampling
+        self.wavelength = wavelength
+        self.maximum_term = maximum_term
+
+    def run(self, model: EyeModel, backend: type[BaseBackend]) -> pd.DataFrame:
+        result = visisipy.analysis.zernike_standard_coefficients(
+            model=model,
+            field_coordinate=self.coordinate,
+            wavelength=self.wavelength,
+            field_type="angle",
+            sampling=self.sampling,
+            maximum_term=self.maximum_term,
+            unit="microns",
+            backend=backend,
+        )
+
+        return pd.DataFrame(result.items(), columns=["coefficient", "value"])
---
+++ scripts/test_data/generate_test_data.py
@@ -0,0 +1,184 @@
+from __future__ import annotations
+
+import sys
+from pathlib import Path
+from typing import TYPE_CHECKING
+from warnings import filterwarnings
+
+import numpy as np
+from analysis_tests import (
+    BaseAnalysisTest,
+    FFTPSFTest,
+    HuygensPSFTest,
+    OPDMapTest,
+    RayTraceTest,
+    RefractionTest,
+    ZernikeStandardCoefficientsTest,
+)
+from loguru import logger
+
+from visisipy.backend import DEFAULT_BACKEND_SETTINGS
+from visisipy.models.base import EyeModel
+from visisipy.models.geometry import BiconicSurface, EyeGeometry, StandardSurface, Stop
+from visisipy.models.materials import NavarroMaterials543
+from visisipy.opticstudio.backend import OpticStudioBackend, OpticStudioSettings
+from visisipy.optiland.backend import OptilandBackend, OptilandSettings
+
+if TYPE_CHECKING:
+    from visisipy.backend import BaseBackend
+
+TEST_DATA_DIR = Path(__file__).parent.parent.parent / "tests" / "_data" / "analysis_results"
+
+DEFAULT_WAVELENGTH = 0.543
+DEFAULT_SAMPLING = 128
+
+OPTICSTUDIO_BACKEND_SETTINGS = OpticStudioSettings(
+    mode="standalone",
+    ray_aiming="off",
+    **DEFAULT_BACKEND_SETTINGS,
+)
+OPTILAND_BACKEND_SETTINGS = OptilandSettings(
+    computation_backend="numpy",
+    **DEFAULT_BACKEND_SETTINGS,
+)
+
+RAY_TRACE_COORDINATES = [(x, y) for x in np.linspace(-60, 60, 5) for y in np.linspace(-60, 60, 5)]
+
+TESTS: dict[str, BaseAnalysisTest] = {
+    "fft_psf_0_0": FFTPSFTest(coordinate=(0, 0), sampling=DEFAULT_SAMPLING, wavelength=DEFAULT_WAVELENGTH),
+    "fft_psf_0_10": FFTPSFTest(coordinate=(0, 10), sampling=DEFAULT_SAMPLING, wavelength=DEFAULT_WAVELENGTH),
+    "fft_psf_10_0": FFTPSFTest(coordinate=(10, 0), sampling=DEFAULT_SAMPLING, wavelength=DEFAULT_WAVELENGTH),
+    "huygens_psf_0_0": HuygensPSFTest(
+        coordinate=(0, 0),
+        pupil_sampling=DEFAULT_SAMPLING,
+        image_sampling=DEFAULT_SAMPLING,
+        wavelength=DEFAULT_WAVELENGTH,
+    ),
+    "huygens_psf_0_10": HuygensPSFTest(
+        coordinate=(0, 10),
+        pupil_sampling=DEFAULT_SAMPLING,
+        image_sampling=DEFAULT_SAMPLING,
+        wavelength=DEFAULT_WAVELENGTH,
+    ),
+    "huygens_psf_10_0": HuygensPSFTest(
+        coordinate=(10, 0),
+        pupil_sampling=DEFAULT_SAMPLING,
+        image_sampling=DEFAULT_SAMPLING,
+        wavelength=DEFAULT_WAVELENGTH,
+    ),
+    "ray_trace_pupil_0_0": RayTraceTest(
+        coordinates=RAY_TRACE_COORDINATES, pupil=(0, 0), wavelength=DEFAULT_WAVELENGTH, sampling=DEFAULT_SAMPLING
+    ),
+    "ray_trace_pupil_0_1": RayTraceTest(
+        coordinates=RAY_TRACE_COORDINATES, pupil=(0, 1), wavelength=DEFAULT_WAVELENGTH, sampling=DEFAULT_SAMPLING
+    ),
+    "ray_trace_pupil_1_-1": RayTraceTest(
+        coordinates=RAY_TRACE_COORDINATES, pupil=(1, -1), wavelength=DEFAULT_WAVELENGTH, sampling=DEFAULT_SAMPLING
+    ),
+    "refraction": RefractionTest(
+        coordinates=[(x, y) for x in range(-10, 11, 5) for y in range(-10, 11, 5)],
+        wavelength=DEFAULT_WAVELENGTH,
+        sampling=DEFAULT_SAMPLING,
+    ),
+    "opd_map_0_0": OPDMapTest(
+        coordinate=(0, 0), wavelength=DEFAULT_WAVELENGTH, sampling=DEFAULT_SAMPLING, remove_tilt=False
+    ),
+    "opd_map_0_10": OPDMapTest(
+        coordinate=(0, 10), wavelength=DEFAULT_WAVELENGTH, sampling=DEFAULT_SAMPLING, remove_tilt=False
+    ),
+    "opd_map_10_0": OPDMapTest(
+        coordinate=(10, 0), wavelength=DEFAULT_WAVELENGTH, sampling=DEFAULT_SAMPLING, remove_tilt=False
+    ),
+    "zernike_coefficients_0_0": ZernikeStandardCoefficientsTest(
+        coordinate=(0, 0), wavelength=DEFAULT_WAVELENGTH, sampling=DEFAULT_SAMPLING, maximum_term=45
+    ),
+    "zernike_coefficients_0_10": ZernikeStandardCoefficientsTest(
+        coordinate=(0, 10), wavelength=DEFAULT_WAVELENGTH, sampling=DEFAULT_SAMPLING, maximum_term=45
+    ),
+    "zernike_coefficients_10_0": ZernikeStandardCoefficientsTest(
+        coordinate=(10, 0), wavelength=DEFAULT_WAVELENGTH, sampling=DEFAULT_SAMPLING, maximum_term=45
+    ),
+}
+
+
+def model() -> EyeModel:
+    """Create the eye model used for generating the test data."""
+    materials = NavarroMaterials543()
+    geometry = EyeGeometry(
+        cornea_front=BiconicSurface(
+            radius=7.6967, asphericity=-0.2304, thickness=0.5615, radius_x=7.9487, asphericity_x=-0.2304
+        ),
+        cornea_back=StandardSurface(radius=6.2343, asphericity=-0.1444, thickness=3.345),
+        pupil=Stop(semi_diameter=1.0),
+        lens_front=StandardSurface(radius=10.2, asphericity=-3.1316, thickness=3.17),
+        lens_back=StandardSurface(radius=-5.4537, asphericity=0, thickness=17.2285),
+        retina=StandardSurface(radius=-12.5000, asphericity=0.033),
+    )
+    return EyeModel(geometry=geometry, materials=materials)
+
+
+def initialize_backends() -> list[type[BaseBackend]]:
+    """Initialize the backends with the default settings for generating the test data."""
+    OpticStudioBackend.initialize(**OPTICSTUDIO_BACKEND_SETTINGS)
+    OptilandBackend.initialize(**OPTILAND_BACKEND_SETTINGS)
+
+    return [OpticStudioBackend, OptilandBackend]
+
+
+def reset_backend_settings(backend: type[BaseBackend]) -> None:
+    """Reset the backend settings to the default values for generating the test data."""
+    if backend is OpticStudioBackend:
+        OpticStudioBackend.update_settings(**OPTICSTUDIO_BACKEND_SETTINGS)
+    elif backend is OptilandBackend:
+        OptilandBackend.update_settings(**OPTILAND_BACKEND_SETTINGS)
+    else:
+        raise ValueError(f"Unknown backend type: {backend}")
+
+
+def get_file_name(test_name: str, backend_name: str, **extra_settings: str) -> Path:
+    """Generate a file name for the test data based on the test name, backend name, and extra settings."""
+    file_name = f"{test_name}_{backend_name}"
+
+    if extra_settings:
+        settings_str = "_".join(f"{key}-{value}" for key, value in extra_settings.items())
+        file_name += f"_{settings_str}"
+
+    return TEST_DATA_DIR / f"{file_name}.csv"
+
+
+def main():
+    backends = initialize_backends()
+    eye_model = model()
+
+    for name, test in TESTS.items():
+        for backend in backends:
+            reset_backend_settings(backend)
+
+            result_path = get_file_name(test_name=name, backend_name=str(backend.type))
+
+            if result_path.exists():
+                logger.info(
+                    "Not generating test data for {} with backend {} already exists at {}.",
+                    name,
+                    backend.type,
+                    result_path,
+                )
+                continue
+
+            logger.info("Running {} with backend {}...", name, backend.type)
+            try:
+                results = test.run(model=eye_model, backend=backend)
+            except Exception as e:  # noqa: BLE001
+                logger.error("Error running {} with backend {}: {}", name, backend.type, e)
+                continue
+
+            results.to_csv(result_path, index=True)
+            logger.success("Results saved to {}", result_path)
+
+
+if __name__ == "__main__":
+    filterwarnings("ignore", category=UserWarning, message="Header and row length mismatch.*")
+
+    logger.remove()  # Remove default logger to avoid duplicate logs
+    logger.add(sys.stdout, format="<level>{level} | {message}</level>", colorize=True)
+    main()
