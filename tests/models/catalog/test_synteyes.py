b054eeb6413fe7a8190bf812977920a7e211c163
crnh <30109443+crnh@users.noreply.github.com>
2026-07-31 12:15:37 +0200

    Update tests for SyntEyes with standard lens front surface

--- tests/models/catalog/test_synteyes.py
+++ tests/models/catalog/test_synteyes.py
@@ -1,14 +1,19 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 import pytest
 
 from tests.test_synteyes import sample_synteye  # noqa: F401
 from visisipy.models.catalog.synteyes import SyntEyesEyeModel, SyntEyesGeometry
 from visisipy.models.materials import EyeMaterials
 
+if TYPE_CHECKING:
+    from visisipy.synteyes import SyntEye3D
+
 
 class TestSyntEyesGeometry:
-    def test_create_synteyes_geometry(self, sample_synteye):  # noqa: F811
+    def test_create_synteyes_geometry(self, sample_synteye: SyntEye3D):  # noqa: F811
         geometry = SyntEyesGeometry(synteye=sample_synteye)
 
         assert geometry.cornea_front.thickness == sample_synteye.biometry.cornea_thickness
@@ -26,8 +31,6 @@ class TestSyntEyesGeometry:
         assert geometry.lens_front.thickness == sample_synteye.biometry.lens_thickness
         assert geometry.lens_front.radius == sample_synteye.lens.anterior_radius
         assert geometry.lens_front.asphericity == sample_synteye.lens.anterior_conic
-        assert geometry.lens_front.zernike_coefficients == sample_synteye.lens.anterior_zernikes
-        assert geometry.lens_front.norm_radius == sample_synteye.lens.anterior_norm_diameter / 2
 
         assert geometry.lens_back.thickness == sample_synteye.biometry.vitreous_depth
         assert geometry.lens_back.radius == sample_synteye.lens.posterior_radius
@@ -39,13 +42,13 @@ class TestSyntEyesGeometry:
 
 
 class TestSyntEyesEyeModel:
-    def test_create_synteyes_eye_model(self, sample_synteye):  # noqa: F811
+    def test_create_synteyes_eye_model(self, sample_synteye: SyntEye3D):  # noqa: F811
         model = SyntEyesEyeModel(synteye=sample_synteye)
 
         assert isinstance(model.geometry, SyntEyesGeometry)
         assert model.geometry.cornea_front.zernike_coefficients == sample_synteye.cornea.anterior_zernikes
         assert model.geometry.cornea_back.zernike_coefficients == sample_synteye.cornea.posterior_zernikes
-        assert model.geometry.lens_front.zernike_coefficients == sample_synteye.lens.anterior_zernikes
+        assert model.geometry.lens_front.thickness == sample_synteye.biometry.lens_thickness
         assert model.geometry.lens_back.thickness == sample_synteye.biometry.vitreous_depth
         assert model.geometry.retina.ellipsoid_radii.x == pytest.approx(sample_synteye.retina.radius_x)
         assert model.geometry.retina.ellipsoid_radii.y == pytest.approx(sample_synteye.retina.radius_y)
